13240b86994ecb1cc6c9055939eabe2209f9d57d
Luis N Diaz <rtvdiaz.antartida@gmail.com>
2025-10-08 11:33:45 -0300

    actualiz

--- src/config/mongoDB.config.js
+++ src/config/mongoDB.config.js
@@ -9,7 +9,7 @@ async function connectMongoDB() {
         await mongoose.connect(ENVIRONMENT.MONGO_DB_CONNECTION_STRING, {
        /*      useNewUrlParser: true, 
             useUnifiedTopology: true,  */
-            timeoutMS: 10000 //10s
+            timeoutMS: 30000 //10s
         })
         console.log('Conexion con MongoDB fue exitosa')
     }
